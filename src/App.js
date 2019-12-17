1820868b682a76676f97fb2d23ed6c3bad568fc2
aoi <s19006@std.it-college.ac.jp>
2019-12-17 15:21:17 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
+const Greeting = props => {
+  return (
+    <h1>[Math.floor(Math.random() * 3)]</h1>
+  )
+}
+
 class App extends React.Component {
-    constructor (props) {
-        super(props)
-        this.state = {}
-    }
-    render () {
-        return (
-        <div></div>
-    }
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div>
+        <Greeting text='こんにちは。' />
+      </div>
+    )
+  }
 }
 export default App
