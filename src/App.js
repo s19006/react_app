9de9f2001c6bb3315b5abb7529095cdaa9be20ac
aoi <s19006@std.it-college.ac.jp>
2019-12-19 15:19:11 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-const embed = props => {
-  const item = 'SDカード'
-  const value = '1200'
+const Embed = props => {
   return (
-    <h1>{item}-{value}円</h1>
+    <h1>{props.item}-{props.value}円</h1>
   )
 }
 
@@ -17,7 +15,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <embed />
+        <Embed item='値段は' value='1200' />
       </div>
     )
   }
