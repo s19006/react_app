fcccfa53ba12ff85acb1b379f728aef5cb213775
aoi <s19006@std.it-college.ac.jp>
2019-12-19 16:40:28 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
+const Start = props => {
+    return (
+        <h1 />
+        )}
 class App extends React.Component {
     constructor (props) {
         super(props)
         this.state = {}
     }
     render () {
-        return (
-        <div></div>
+        return ()
+            <div />
     }
 }
 export default App
